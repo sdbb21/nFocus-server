d2c277708fda823a4f3f13545609805322381671
Sebastian Baez <sdbb.unl@gmail.com>
2020-06-30 16:12:12 +0200

    Score query with User Name, done

--- routers/scorelist.js
+++ routers/scorelist.js
@@ -2,15 +2,15 @@ const express = require("express");
 const { Router } = express;
 const router = new Router();
 const User = require("../models").user;
-const ScoreList = require("../models").scorelist;
+const ScoreList = require("../models").scoreList;
 
 // GET the 10 highest Scores with User names
 router.get("/", async (req, res, next) => {
   try {
     const get10Scores = await ScoreList.findAll({
-      limit: 10,
-      //   include: [User],
-      // order: "score DESC",
+      include: [{ model: User, attributes: ["name"] }],
+      order: [["score", "DESC"]],
+      limit: 2,
     });
 
     res.json(get10Scores);
